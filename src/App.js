f9aee002a9e96bcf51e0ab24b3a69b4efb2e44e9
vitorio-p <vitorio.pandinata@foodpanda.com>
2024-04-02 01:19:18 +0800

    create base UI + implement add button functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const storage = ["iPhone 15 Pro", "Macbook Air M3"];
+  const [items, setItems] = useState(storage);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const input1 = document.getElementById('input1').value
+    const newCurrentList = [...items, input1];
+    setItems(newCurrentList);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>wishlist</h1>
+      <form onSubmit={handleSubmit}>
+        <input id="input1" type="text"></input>
+        <button type="submit">
+          add new wish
+        </button>
+      </form>
+      <h2>current list</h2>
+      {items.map((element, id) => {
+        return <p key={id}>{element}</p>;
+      })}
     </div>
   );
 }
